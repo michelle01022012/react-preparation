c1ffdc832f9e15df1909192b8dd5e3dea3ad3bcb
Michelle Anderson <manderson0102@yahoo.com>
2026-04-26 00:28:15 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { BrowserRouter as Router, Routes, Rout } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Fixed 'Route' typo
 import Home from './pages/Home';
+import Posts from './pages/Posts'; // Ensure this is imported
+
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <Router>
+      <div className="App">
         <Routes>
-          <Route path="/" element={<Home />}</Route>
-          <Route path=":id" element={<Posts />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<Posts />} />
         </Routes>
       </div>
     </Router>        
