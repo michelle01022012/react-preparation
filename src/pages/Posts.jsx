109b1a706cd5e25433dc8c476b7ca9ad610254ca
Michelle Anderson <manderson0102@yahoo.com>
2026-05-03 05:15:43 -0400

    commit

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
 
-Const Posts = () => {
+const Posts = () => {
     const params = useParams();
     console.log(params);
   return (
