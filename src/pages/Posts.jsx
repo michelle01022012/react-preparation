b5db3ddc0ba7b766922c02ffb5a6e416a36388da
Michelle Anderson <manderson0102@yahoo.com>
2026-05-05 17:28:43 -0400

    post updates

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -32,7 +32,7 @@ const Posts = () => {
        <div className="post">
         <div className="post__title">
           <div className="post__title--skeleton"></div>
-        </div> className="post__body">
+         <div className="post__body">
           <p className="post__body--skeleton"></p>
         </div>
       </div>
