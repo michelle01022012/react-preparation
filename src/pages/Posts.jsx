01b1cb1bec4513c7f9ea1400bd1320e6f44113fa
Michelle Anderson <manderson0102@yahoo.com>
2026-05-05 12:42:07 -0400

    axios

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { use, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const Posts = () => {
     const { id } = useParams();
-    console.log(id);
+
+    useEffect(() => {
+      async function fetchPosts() {
+      const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    console.log(data)
+    const data2 = await data.json()
+    console.log(data2)
+      }
+      fetchPosts();
+      }, []);
   return (
     <div>
-      Posts      
+      {id}   
     </div>
   );
 }
