1d9f055083de327756251d72d2fd699c133d8822
Michelle Anderson <manderson0102@yahoo.com>
2026-05-05 17:17:49 -0400

    updated posts

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -5,19 +5,48 @@ import axios from 'axios';
 const Posts = () => {
     const { id } = useParams();
     const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
       async function fetchPosts() {
-      const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts?userId=${id}');
+      const { data } = await axios.get("https://jsonplaceholder.typicode.com/posts?userId=2");
+      console.log(data);
       setPosts(data);
+      setLoading(false);
       }
       fetchPosts();
       }, []);
   return (
-    <div>
-      {posts.map(post => <div>{post.id}</div>)}
+   <>
+  <div className="post__search">
+    <button>← Back</button>
+    <div className="post__search--container">
+      <label className="post__search--label">Search by Id</label>
+      <input
+        type="number"
+      />
+      <button>Enter</button>
     </div>
+  </div>
+     {loading ? (
+       <div className="post">
+        <div className="post__title">
+          <div className="post__title--skeleton"></div>
+        </div> className="post__body">
+          <p className="post__body--skeleton"></p>
+        </div>
+      </div>
+      ) : (
+      posts.map((post) => (
+      <div className="post">
+      <div className="post__title">{post.title}</div>
+      <p className="post__body">{post.body}</p>
+    </div>
+    ))
+      )}
+    {}   
+    </>
   );
-}
+};
 
 export default Posts;
