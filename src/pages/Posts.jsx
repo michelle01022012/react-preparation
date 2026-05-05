af426389a16a524c50edad7cf5df6853fffe485a
Michelle Anderson <manderson0102@yahoo.com>
2026-05-05 19:09:06 -0400

    Link

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,60 +1,74 @@
-import React, { use, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const Posts = () => {
-    const { id } = useParams();
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [searchId, setSearchId] = useState(id);
+  const { id } = useParams();
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState();
+  const [searchId, setSearchId] = useState(id);
 
-   function onSearch() {
+  function onSearch() {
     fetchPosts(searchId);
-     }
-      async function fetchPosts(userId) {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId || id}`);
-        const data = response.data; 
-      setPosts(data);
-      setLoading(false);
-      } 
+  }
 
-    useEffect(() => {
-      fetchPosts();
-      }, []);
+  async function fetchPosts(userId) {
+    setLoading(true);
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts?userId=${userId || id}`
+    );
+
+    const data = response.data;
+    setPosts(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
-   <>
-  <div className="post__search">
-    <button>← Back</button>
-    <div className="post__search--container">
-      <label className="post__search--label">Search by Id</label>
-      <input type="number" 
-      value={searchId}
-      onChange={(event) => setSearchId(event.target.value)} 
-      onKeyPress={(event) => {
-        if (event.key === 'Enter') {
-          onSearch();
-        }
-     />
-      <button onClick={() => onSearch()}>Enter</button>
-    </div>
-  </div>
-  {loading 
-       ? new Array(10).fill(0).map((_, index) => (
-        <div className="Post" key={index}>
-          <div className="post__title">
-            <div className="post__title--skeleton"></div>
-        </div>
-        <div className="post__body">
-          <p className="post__body--skeleton"></p>
+    <>
+      <div className="post__search">
+        <Link to="/">
+        <button>← Back</button>
+        </Link>
+        <div className="post__search--container">
+          <label className="post__search--label">Search by Id</label>
+
+          <input
+            type="number"
+            value={searchId}
+            onChange={(event) => setSearchId(event.target.value)}
+            onKeyPress={(event) => {
+              if (event.key === 'Enter') {
+                onSearch();
+              }
+            }}
+          />
+
+          <button onClick={() => onSearch()}>Enter</button>
         </div>
       </div>
-       ))
-       : posts.map(post => (
-         <div className="post" key={post.id}>
-           <div className="post__title">{post.title}</div>
-           <p className="post__body">{post.body}</p>
-        </div>
-        ))}
+
+      {loading
+        ? new Array(10).fill(0).map((_, index) => (
+            <div className="post" key={index}>
+              <div className="post__title">
+                <div className="post__title--skeleton"></div>
+              </div>
+
+              <div className="post__body">
+                <p className="post__body--skeleton"></p>
+              </div>
+            </div>
+          ))
+        : posts.map((post) => (
+            <div className="post" key={post.id}>
+              <div className="post__title">{post.title}</div>
+              <p className="post__body">{post.body}</p>
+            </div>
+          ))}
     </>
   );
 };
